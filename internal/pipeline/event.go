1a0a0e5c3efa2253a64cf59516f6b2d63fceef00
Momchil Atanasov <momchil.dev@gmail.com>
2026-04-06 01:53:46 +0300

    Rework lifecycle node into invalidation node

--- internal/pipeline/event.go
+++ internal/pipeline/event.go
@@ -3,3 +3,14 @@ package pipeline
 type ChangeEvent struct {
 	Paths []string
 }
+
+// InvalidationEvent indicates that the application needs to be restarted and
+// optionally rebuilt as well.
+type InvalidationEvent struct {
+
+	// ShouldRebuild indicates whether the application should be rebuilt before
+	// being restarted. If false, the application will be restarted with the
+	// currently available binary. If true, the application will be rebuilt before
+	// being restarted.
+	ShouldRebuild bool
+}
