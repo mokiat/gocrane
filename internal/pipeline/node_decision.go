dc3f2ef8dcae172c0989e383b8fcecce5678fdd9
Momchil Atanasov <momchil.dev@gmail.com>
2026-04-09 23:55:38 +0300

    Implement Decision Node

--- internal/pipeline/node_decision.go
+++ internal/pipeline/node_decision.go
@@ -1,21 +1,48 @@
 package pipeline
 
-import "context"
+import (
+	"context"
 
-// TODO: Write tests for this node.
+	"github.com/mokiat/gog/filter"
+	"github.com/mokiat/gog/opt"
+)
 
 // NewDecisionNode creates a new DecisionNode with the specified arguments.
-func NewDecisionNode() *DecisionNode {
-	// TODO: Take filter trees as arguments.
-	return &DecisionNode{}
+func NewDecisionNode(rebuildFilter, restartFilter filter.Func[string]) *DecisionNode {
+	return &DecisionNode{
+		rebuildFilter: rebuildFilter,
+		restartFilter: restartFilter,
+	}
 }
 
 // DecisionNode is responsible for deciding whether a change event should
 // trigger a restart event and whether that restart should include a rebuild
 // as well.
-type DecisionNode struct{}
+type DecisionNode struct {
+	rebuildFilter filter.Func[string]
+	restartFilter filter.Func[string]
+}
 
 // Run starts the decision node.
 func (n *DecisionNode) Run(ctx context.Context, inEvents Queue[ChangeEvent], outEvents Queue[RestartEvent]) error {
+	var changeEvent ChangeEvent
+	for inEvents.Pop(ctx, &changeEvent) {
+		var optOutEvent opt.T[RestartEvent]
+		switch {
+		case n.rebuildFilter(changeEvent.Path):
+			optOutEvent = opt.V(RestartEvent{
+				ShouldRebuild: true,
+			})
+		case n.restartFilter(changeEvent.Path):
+			optOutEvent = opt.V(RestartEvent{
+				ShouldRebuild: false,
+			})
+		}
+		if outEvent, ok := optOutEvent.Unwrap(); ok {
+			if !outEvents.Push(ctx, outEvent) {
+				return nil
+			}
+		}
+	}
 	return nil
 }
